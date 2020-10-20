899dcfe26a756c19d69850a6e703cfbf18e11ba7
George Rosescu <george.rosescu@gmail.com>
2020-10-20 14:34:47 +0300

    Initial Commit

---
+++ Check✓/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Check✓
+//
+//  Created by George Rosescu on 20/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
