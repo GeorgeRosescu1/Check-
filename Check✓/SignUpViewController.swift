cca2ba8690a34246dc134c160ee2c0f0d72f5dba
George Rosescu <george.rosescu@gmail.com>
2020-10-27 15:55:04 +0200

    material textfields and design updates

--- Check✓/SignUpViewController.swift
+++ Check✓/SignUpViewController.swift
@@ -7,25 +7,30 @@
 
 import UIKit
 import MaterialComponents.MaterialTextFields
-import RAGTextField
+
 
 class SignUpViewController: UIViewController {
-
+    
     @IBOutlet weak var signUpButton: UIButton!
-    @IBOutlet weak var passwordMaterialTextField: RAGTextField!
-    @IBOutlet weak var emailMaterialTextField: RAGTextField!
+    @IBOutlet weak var logoImageView: UIImageView!
+    
+    
+    var emailMaterialTextField = MDCOutlinedTextField(frame: CGRect(x: 33, y: 263, width: 349, height: 34))
+    var passwordMaterialTextField = MDCOutlinedTextField(frame: CGRect(x: 33, y: 354, width: 349, height: 34))
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        passwordMaterialTextField.delegate = self
-        emailMaterialTextField.delegate = self
         
         configureUI()
         
     }
-
+    
     @IBAction func signUpAction(_ sender: UIButton) {
         print("Sign up")
+    }
+    
+    @IBAction func backToLogin(_ sender: UIButton) {
+        self.navigationController?.popViewController(animated: true)
     }
     
     private func configureUI() {
@@ -35,29 +40,43 @@ class SignUpViewController: UIViewController {
         signUpButton.isEnabled = false
         signUpButton.alpha = 0.8
         
-        passwordMaterialTextField.placeholderColor = .white
-        passwordMaterialTextField.textPadding = UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
-        passwordMaterialTextField.borderStyle = .roundedRect
-        passwordMaterialTextField.textColor = .white
-       // passwordMaterialTextField.
+        logoImageView.layer.cornerRadius = 8
         
-        emailMaterialTextField.textColor = .white
+        emailMaterialTextField.label.text = "Email address"
+        emailMaterialTextField.setFloatingLabelColor(.white, for: .editing)
+        emailMaterialTextField.setFloatingLabelColor(.white, for: .normal)
+        emailMaterialTextField.setNormalLabelColor(.gray, for: .normal)
+        emailMaterialTextField.placeholder = "example@mail.com"
+        emailMaterialTextField.leadingAssistiveLabel.text = "Your email address. You will log in with it."
+        emailMaterialTextField.setLeadingAssistiveLabelColor(.white, for: .editing)
+        emailMaterialTextField.setLeadingAssistiveLabelColor(.white, for: .normal)
+        emailMaterialTextField.leadingAssistiveLabel.isHidden = true
+        emailMaterialTextField.setOutlineColor(.gray, for: .normal)
+        emailMaterialTextField.setOutlineColor(.white, for: .editing)
+        emailMaterialTextField.setTextColor(.white, for: .normal)
+        emailMaterialTextField.setTextColor(.white, for: .editing)
+        emailMaterialTextField.sizeToFit()
+        self.view.addSubview(emailMaterialTextField)
+        //make custom view with xib to use this in other screens as well
+        
+        passwordMaterialTextField.label.text = "Password"
+        passwordMaterialTextField.setFloatingLabelColor(.white, for: .editing)
+        passwordMaterialTextField.setFloatingLabelColor(.white, for: .normal)
+        passwordMaterialTextField.setNormalLabelColor(.gray, for: .normal)
+        passwordMaterialTextField.leadingAssistiveLabel.text = "Min 6 characters, 1 digit and capital letter."
+        passwordMaterialTextField.leadingAssistiveLabel.isHidden = true
+        passwordMaterialTextField.setLeadingAssistiveLabelColor(.white, for: .editing)
+        passwordMaterialTextField.setLeadingAssistiveLabelColor(.white, for: .normal)
+        passwordMaterialTextField.setTextColor(.white, for: .normal)
+        passwordMaterialTextField.setTextColor(.white, for: .editing)
+        passwordMaterialTextField.setOutlineColor(.gray, for: .normal)
+        passwordMaterialTextField.setOutlineColor(.white, for: .editing)
+        passwordMaterialTextField.sizeToFit()
+        self.view.addSubview(passwordMaterialTextField)
     }
+    
 }
 
 extension SignUpViewController: UITextFieldDelegate {
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        print(textField.text)
-    }
     
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        if textField === emailMaterialTextField {
-            passwordMaterialTextField.becomeFirstResponder()
-        }
-        return true
-    }
-    
-    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
-        return true
-    }
 }
